8285ca9b861a5154f64496801ac904853d0f8060
Javier-Acosta <jacosta@catamarca.gov.ar>
2022-05-20 19:45:49 -0300

    EjercicioClase6

--- ejercicioClase5/Program.cs
+++ ejercicioClase5/Program.cs
@@ -41,10 +41,10 @@
 
 
 
+//------------------------------------------------------------
 
 
-
-
+//ejemplo Alternativo
 
 
 //1) Borrar la pantalla.
@@ -56,29 +56,153 @@
 //7) Si la respuesta no es ni "S" ni "N" que tambien finalice el programa, pero mostrando un mensaje que diga "Opcion no valida".
 
 
-var opcionElegida = "S";
+//var opcionElegida = "S";
 
-while (opcionElegida.ToUpper()=="S")
-{
-    //borrar la pantalla
-    Console.Clear();
-    //pekdir nombre
-    Console.WriteLine("Cual es su nobre?");
-    var nombrePersona = Console.ReadLine();
+//while (opcionElegida.ToUpper()=="S")
+//{
+//    //borrar la pantalla
+//    Console.Clear();
+//    //pekdir nombre
+//    Console.WriteLine("Cual es su nobre?");
+//    var nombrePersona = Console.ReadLine();
 
-    Console.WriteLine($"¡Hola {nombrePersona}!");
-    Console.WriteLine("Desea continuar? S/N");
-    opcionElegida = Console.ReadLine();
+//    Console.WriteLine($"¡Hola {nombrePersona}!");
+//    Console.WriteLine("Desea continuar? S/N");
+//    opcionElegida = Console.ReadLine();
+//}
+
+//if (opcionElegida.ToUpper() == "N")
+//{
+//    Console.WriteLine("Programa finalizado correctamente");
+
+//}
+
+//else
+//{
+//    Console.WriteLine("Opcion no valida");
+//}
+
+
+
+
+
+//------------------------------------ Alternativo con Do While -------------------------------------------------
+
+
+
+//string opcionElegida;
+//do
+//{
+//    //borrar la pantalla
+//    Console.Clear();
+//    //pekdir nombre
+//    Console.WriteLine("Cual es su nobre?");
+//    var nombrePersona = Console.ReadLine();
+
+//    Console.WriteLine($"¡Hola {nombrePersona}!");
+//    Console.WriteLine("Desea continuar? S/N");
+//    opcionElegida = Console.ReadLine();
+//}
+//while (opcionElegida.ToUpper() == "S");
+
+//if (opcionElegida.ToUpper() == "N")
+//{
+//    Console.WriteLine("Programa finalizado correctamente");
+
+//}
+
+//else
+//{
+//    Console.WriteLine("Opcion no valida");
+//}
+
+//-------------------------------------------------------------------------------------- Ejercicio 5 -------------------------------------------------
+//1) Generar un número secreto
+//aleatorio con la siguiente instruccion:
+
+//// Genera un numero entero aleatorio de 1 a 20
+//int numeroSecreto = new
+//Random(DateTime.Now.Millisecond).Next(1, 21);
+
+//2) Pedir al usuario que ingrese un número y que
+//intente adivinar el número que eligió la computadora.
+
+//3) Si el numero ingresado por el usuario es mayor
+//al número secreto, avisarle que es muy grande y que intente de nuevo y que
+//vuelva al paso 2.
+
+//4) Si el numero ingresado es menor al número
+//secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
+
+//5) Si el número ingresado coincide con el número
+//secreto, sacar el siguiente mensaje:
+
+//"Felicitaciones, has adivinado el número
+//secreto que era:  [numeroSecreto] "
+//"Lo has logrado en [n] intentos!!"
+
+//Y finalizar el programa.
+
+//Nota:
+//Obviamente hay que hacer un bucle y crear un contador de intentos.
+//Sean honestos con ustedes mismos y usen sus propias neuronas para resolver el problema, y aunque no lo terminen lo importante es intentarlo.
+
+int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+int contador=0;
+//pekdir nombre
+Console.WriteLine("Ingresa un número");
+int numeroIngresado =  int.Parse(Console.ReadLine());
+
+
+while (numeroIngresado == numeroSecreto)
+    contador++;
+
+if (numeroIngresado > numeroSecreto)
+    {
+        
+        while (numeroIngresado > numeroSecreto)
+        {
+            
+            Console.WriteLine("Es mas abajo");
+            Console.WriteLine("Elige otro numero menor");
+            numeroIngresado = int.Parse(Console.ReadLine());
+            
+        }
+
+        if (numeroIngresado < numeroSecreto)
+        {
+            while (numeroIngresado < numeroSecreto)
+            {
+
+                Console.WriteLine("el numero secreto es muy mayor");
+                Console.WriteLine("Elige otro numero mayor");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+            }
+
+        }
+    
 }
-
-if (opcionElegida.ToUpper() == "N")
+if (numeroIngresado < numeroSecreto)
 {
-    Console.WriteLine("Programa finalizado correctamente");
+    while (numeroIngresado < numeroSecreto)
+    {
+
+        Console.WriteLine("el numero secreto es muy mayor");
+        Console.WriteLine("Elige otro numero mayor");
+        numeroIngresado = int.Parse(Console.ReadLine());
+
+    }
 
 }
+Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto} ");
+Console.WriteLine($"Lo has logrado en {contador} intentos!!");
 
-else
-{
-    Console.WriteLine("Opcion no valida");
-}
+
+
+
+
+
+
 
